140b9546e33c87e12d83b4ea5436258e1a64346a
Jonas Follesø <jonas@follesoe.no>
2015-11-05 21:12:11 +0100

    Add script to open port 80 on host machine.

---
+++ Firewall.ps1
@@ -0,0 +1,3 @@
+if (!(Get-NetFirewallRule | where {$_.Name -eq "TCP80"})) {
+    New-NetFirewallRule -Name "TCP80" -DisplayName "HTTP on TCP/80" -Protocol tcp -LocalPort 80 -Action Allow -Enabled True
+}
