02a1511a46217b0f00fc5ec4e9f227bed381573c
Jonas Follesø <jonas@follesoe.no>
2015-11-05 22:06:38 +0100

    Container and Host share firewall settings - need to open both 80 and 5004.

--- Firewall.ps1
+++ Firewall.ps1
@@ -1,3 +1,7 @@
 if (!(Get-NetFirewallRule | where {$_.Name -eq "TCP80"})) {
     New-NetFirewallRule -Name "TCP80" -DisplayName "HTTP on TCP/80" -Protocol tcp -LocalPort 80 -Action Allow -Enabled True
 }
+
+if (!(Get-NetFirewallRule | where {$_.Name -eq "TCP5004"})) {
+    New-NetFirewallRule -Name "TCP5004" -DisplayName "HTTP on TCP/5004" -Protocol tcp -LocalPort 5004 -Action Allow -Enabled True
+}
